05ebee3095fc69a73b322ac751e33bee99c018e2
naman0058 <jnaman345@gmail.com>
2022-02-03 14:06:45 +0530

    a

--- routes/api.js
+++ routes/api.js
@@ -120,12 +120,29 @@ router.post('/save_my_day',(req,res)=>{
     body['dates'] = today;
 console.log(req.body)
 console.log(final.toString());
-    pool.query(`insert into reports set ?`,body,(err,result)=>{
-        if(err) throw err;
-        else res.json({
-            userdata : result
+
+   pool.query(`select * from reports where dates = '${today}' and userid = '${req.body.userid}' `,(err,result)=>{
+       if(err) throw err;
+       else if(result[0]){
+        pool.query(`update reports set ? where userid = ? `, [req.body, req.body.userid  ], (err, result) => {
+
+            if(err) throw err;
+            else res.json({
+                userdata : result
+            })
+        }) 
+       }
+       else {
+        pool.query(`insert into reports set ?`,body,(err,result)=>{
+            if(err) throw err;
+            else res.json({
+                userdata : result
+            })
         })
-    })
+       }
+   })
+
+ 
 })
 
 
@@ -134,31 +151,430 @@ console.log(final.toString());
 
 
 router.post('/save_das28_esr', (req, res) => {
-    pool.query(`update reports set ? where id = ?`, [req.body, req.body.id], (err, result) => {
-        if(err) {
-            res.json({
-                status:500,
-                type : 'error',
-                description:err
-            })
+    let body = req.body;
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    
+    today = yyyy + '-' + mm + '-' + dd;
+    console.log(req.body)
+
+    pool.query(`select * from reports where dates = '${today}' and userid = '${req.body.userid}'`,(err,result)=>{
+      if(err) throw err;
+      else if(result[0]){
+        body['esr'] = 0.56*Math.sqrt(+req.body.tjc) +0.28*Math.sqrt(+req.body.sjc)+0.014*(+req.body.patient_g_activity)+0.70*(+req.body.esr);
+
+        if(req.body.language == 'en'){
+            if(req.body.esr < 2.6){
+                body['esr_status'] = 'Disease in Remission';
+                body['esr_color'] = 'green';
+        
+                
+            } 
+            else if(req.body.esr > 2.6){
+                body['esr_status'] = 'Low Disease Activity';
+                body['esr_color'] = 'yellow';
+        
+            } 
+            else if(req.body.esr > 3.2){
+                body['esr_status'] = 'Moderate Disease Activity';
+                body['esr_color'] = 'lightred';
+        
+            } 
+            else if(req.body.esr > 5.1){
+                body['esr_status'] = 'High Disease Activity';
+                body['esr_color'] = 'red';
+        
+            } 
         }
         else {
-            res.json({
-                status:200,
-                type : 'success',
-                description:'successfully update'
-            })
-
-            
+            if(req.body.esr < 2.6){
+                body['esr_status'] = 'المرض ساكن (غير نشط)';
+                body['esr_color'] = 'green';
+        
+                
+            } 
+            else if(req.body.esr > 2.6){
+                body['esr_status'] = 'Low نشاط المرض';
+                body['esr_color'] = 'yellow';
+        
+            } 
+            else if(req.body.esr > 3.2){
+                body['esr_status'] = 'Moderate نشاط المرض';
+                body['esr_color'] = 'lightred';
+        
+            } 
+            else if(req.body.esr > 5.1){
+                body['esr_status'] = 'High نشاط المرض';
+                body['esr_color'] = 'red';
+        
+            } 
         }
+    
+    
+        console.log(req.body)
+    
+        pool.query(`update reports set ? where userid = ? and dates = '${today}'`, [req.body, req.body.userid  ], (err, result) => {
+         
+            if(err) {
+                res.json({
+                    status:500,
+                    type : 'error',
+                    description:err
+                })
+            }
+            else {
+                res.json({
+                    status:200,
+                    type : 'success',
+                    description:'successfully update',
+                    result:req.body.esr,
+                    color:req.body.esr_color,
+                    text:req.body.esr_status
+                })
+    
+                
+            }
+        })
+      }
+      else {
+        body['esr'] = 0.56*Math.sqrt(+req.body.tjc) +0.28*Math.sqrt(+req.body.sjc)+0.014*(+req.body.patient_g_activity)+0.70*(+req.body.esr);
+
+        if(req.body.language == 'en'){
+            if(req.body.esr < 2.6){
+                body['esr_status'] = 'Disease in Remission';
+                body['esr_color'] = 'green';
+        
+                
+            } 
+            else if(req.body.esr > 2.6){
+                body['esr_status'] = 'Low Disease Activity';
+                body['esr_color'] = 'yellow';
+        
+            } 
+            else if(req.body.esr > 3.2){
+                body['esr_status'] = 'Moderate Disease Activity';
+                body['esr_color'] = 'lightred';
+        
+            } 
+            else if(req.body.esr > 5.1){
+                body['esr_status'] = 'High Disease Activity';
+                body['esr_color'] = 'red';
+        
+            } 
+        }
+        else {
+            if(req.body.esr < 2.6){
+                body['esr_status'] = 'المرض ساكن (غير نشط)';
+                body['esr_color'] = 'green';
+        
+                
+            } 
+            else if(req.body.esr > 2.6){
+                body['esr_status'] = 'Low نشاط المرض';
+                body['esr_color'] = 'yellow';
+        
+            } 
+            else if(req.body.esr > 3.2){
+                body['esr_status'] = 'Moderate نشاط المرض';
+                body['esr_color'] = 'lightred';
+        
+            } 
+            else if(req.body.esr > 5.1){
+                body['esr_status'] = 'High نشاط المرض';
+                body['esr_color'] = 'red';
+        
+            } 
+        }
+       body['dates'] = today;
+    
+        console.log(req.body)
+    
+        pool.query(`insert into reports set ?`,body, (err, result) => {
+
+         
+            if(err) {
+                res.json({
+                    status:500,
+                    type : 'error',
+                    description:err
+                })
+            }
+            else {
+                res.json({
+                    status:200,
+                    type : 'success',
+                    description:'successfully update',
+                    result:req.body.esr,
+                    color:req.body.esr_color,
+                    text:req.body.esr_status
+                })
+    
+                
+            }
+        })
+      }
+
     })
+
+   
     
 })
 
 
 router.post('/save_das28_crp', (req, res) => {
-    pool.query(`update reports set ? where id = ?`, [req.body, req.body.id], (err, result) => {
+    let body = req.body;
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    
+    today = yyyy + '-' + mm + '-' + dd;
+
+
+ 
+    pool.query(`select * from reports where dates = '${today}' and userid = '${req.body.userid}'`,(err,result)=>{
+     if(err) throw err;
+     else if(result[0]){
+        body['crp'] = 0.56*Math.sqrt(+req.body.tjc) +0.28*Math.sqrt(+req.body.sjc)+0.014*(+req.body.patient_g_activity)+0.36*(+req.body.crp+1)+0.96;
+
+
+        if(req.body.language == 'en'){
+            if(req.body.crp < 2.6){
+                body['crp_status'] = 'Disease in Remission';
+                body['crp_color'] = 'green';
+        
+                
+            } 
+            else if(req.body.crp > 2.6){
+                body['crp_status'] = 'Low Disease Activity';
+                body['crp_color'] = 'yellow';
+        
+            } 
+            else if(req.body.crp > 3.2){
+                body['crp_status'] = 'Moderate Disease Activity';
+                body['crp_color'] = 'lightred';
+        
+            } 
+            else if(req.body.crp > 5.1){
+                body['crp_status'] = 'High Disease Activity';
+                body['crp_color'] = 'red';
+        
+            } 
+        }
+        else {
+            if(req.body.crp < 2.6){
+                body['crp_status'] = 'المرض ساكن (غير نشط)';
+                body['crp_color'] = 'green';
+        
+                
+            } 
+            else if(req.body.crp > 2.6){
+                body['crp_status'] = 'Low نشاط المرض';
+                body['crp_color'] = 'yellow';
+        
+            } 
+            else if(req.body.crp > 3.2){
+                body['crp_status'] = 'Moderate نشاط المرض';
+                body['crp_color'] = 'lightred';
+        
+            } 
+            else if(req.body.crp > 5.1){
+                body['crp_status'] = 'High نشاط المرض';
+                body['crp_color'] = 'red';
+        
+            } 
+        }
+    
+        console.log(req.body)
+    
+        pool.query(`update reports set ? where userid = ? and dates = '${today}'`, [req.body, req.body.userid  ], (err, result) => {
+         
+            if(err) {
+                res.json({
+                    status:500,
+                    type : 'error',
+                    description:err
+                })
+            }
+            else {
+                res.json({
+                    status:200,
+                    type : 'success',
+                    description:'successfully update',
+                    result:req.body.crp,
+                    color:req.body.crp_color,
+                    text:req.body.crp_status
+                })
+    
+                
+            }
+        })
+     }
+     else{
+
+        body['crp'] = 0.56*Math.sqrt(+req.body.tjc) +0.28*Math.sqrt(+req.body.sjc)+0.014*(+req.body.patient_g_activity)+0.36*(+req.body.crp+1)+0.96;
+
+
+        if(req.body.language == 'en'){
+            if(req.body.crp < 2.6){
+                body['crp_status'] = 'Disease in Remission';
+                body['crp_color'] = 'green';
+        
+                
+            } 
+            else if(req.body.crp > 2.6){
+                body['crp_status'] = 'Low Disease Activity';
+                body['crp_color'] = 'yellow';
+        
+            } 
+            else if(req.body.crp > 3.2){
+                body['crp_status'] = 'Moderate Disease Activity';
+                body['crp_color'] = 'lightred';
+        
+            } 
+            else if(req.body.crp > 5.1){
+                body['crp_status'] = 'High Disease Activity';
+                body['crp_color'] = 'red';
+        
+            } 
+        }
+        else {
+            if(req.body.crp < 2.6){
+                body['crp_status'] = 'المرض ساكن (غير نشط)';
+                body['crp_color'] = 'green';
+        
+                
+            } 
+            else if(req.body.crp > 2.6){
+                body['crp_status'] = 'Low نشاط المرض';
+                body['crp_color'] = 'yellow';
+        
+            } 
+            else if(req.body.crp > 3.2){
+                body['crp_status'] = 'Moderate نشاط المرض';
+                body['crp_color'] = 'lightred';
+        
+            } 
+            else if(req.body.crp > 5.1){
+                body['crp_status'] = 'High نشاط المرض';
+                body['crp_color'] = 'red';
+        
+            } 
+        }
+    
+        console.log(req.body)
+
+        body['dates'] = today
+    
+        pool.query(`insert into reports set ?`,body, (err, result) => {
+
+         
+            if(err) {
+                res.json({
+                    status:500,
+                    type : 'error',
+                    description:err
+                })
+            }
+            else {
+                res.json({
+                    status:200,
+                    type : 'success',
+                    description:'successfully update',
+                    result:req.body.crp,
+                    color:req.body.crp_color,
+                    text:req.body.crp_status
+                })
+    
+                
+            }
+        })
+
+
+     }
+
+    })
+
+    
+    
+})
+
+
+router.post('/save_cdai', (req, res) => {
+    let body = req.body;
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    
+    today = yyyy + '-' + mm + '-' + dd;
+
+    pool.query(`select * from reports where dates = '${today}' and userid = '${req.body.userid}'`,(err,result)=>{
+        if(err) throw err;
+        else if(result[0]){
+
+            body['cdai'] = ((+req.body.tjc)+ (+req.body.sjc) + (+req.body.provider_g_activity) + (+req.body.patient_g_activity));
+
+
+    if(req.body.language == 'en'){
+        if(req.body.cdai < 2.8){
+            body['cdai_status'] = 'Disease in Remission';
+            body['cdai_color'] = 'green';
+    
+            
+        } 
+        else if(req.body.cdai > 2.8){
+            body['cdai_status'] = 'Low Disease Activity';
+            body['cdai_color'] = 'yellow';
+    
+        } 
+        else if(req.body.cdai > 10){
+            body['cdai_status'] = 'Moderate Disease Activity';
+            body['cdai_color'] = 'lightred';
+    
+        } 
+        else if(req.body.cdai > 22){
+            body['cdai_status'] = 'High Disease Activity';
+            body['cdai_color'] = 'red';
+    
+        } 
+    }
+    else {
+        if(req.body.cdai < 2.8){
+            body['cdai_status'] = 'المرض ساكن (غير نشط)';
+            body['cdai_color'] = 'green';
+    
+            
+        } 
+        else if(req.body.cdai > 2.8){
+            body['cdai_status'] = 'Low نشاط المرض';
+            body['cdai_color'] = 'yellow';
+    
+        } 
+        else if(req.body.cdai > 10){
+            body['cdai_status'] = 'Moderate نشاط المرض';
+            body['cdai_color'] = 'lightred';
+    
+        } 
+        else if(req.body.cdai > 22){
+            body['cdai_status'] = 'High نشاط المرض';
+            body['cdai_color'] = 'red';
+    
+        } 
+    }
+
+    
+
+    console.log(req.body)
+
+    pool.query(`update reports set ? where userid = ? and dates = '${today}'`, [req.body, req.body.userid  ], (err, result) => {
+
+
         if(err) {
+            console.log(err)
+
             res.json({
                 status:500,
                 type : 'error',
@@ -166,41 +582,107 @@ router.post('/save_das28_crp', (req, res) => {
             })
         }
         else {
+            console.log(result)
             res.json({
                 status:200,
                 type : 'success',
-                description:'successfully update'
+                description:'successfully update',
+                result:req.body.cdai,
+                color:req.body.cdai_color,
+                text:req.body.cdai_status
             })
 
             
         }
     })
     
-})
 
-
-router.post('/save_cdai', (req, res) => {
-    pool.query(`update reports set ? where id = ? and dates = ?`, [req.body, req.body.id , today], (err, result) => {
-        if(err) {
-            res.json({
-                status:500,
-                type : 'error',
-                description:err
-            })
         }
         else {
-            res.json({
-                status:200,
-                type : 'success',
-                description:'successfully update',
-                result:5.22524,
-                color:'red',
-                text:'perfect'
-            })
 
+            body['cdai'] = ((+req.body.tjc)+ (+req.body.sjc) + (+req.body.provider_g_activity) + (+req.body.patient_g_activity));
+
+
+            if(req.body.language == 'en'){
+                if(req.body.cdai < 2.8){
+                    body['cdai_status'] = 'Disease in Remission';
+                    body['cdai_color'] = 'green';
             
+                    
+                } 
+                else if(req.body.cdai > 2.8){
+                    body['cdai_status'] = 'Low Disease Activity';
+                    body['cdai_color'] = 'yellow';
+            
+                } 
+                else if(req.body.cdai > 10){
+                    body['cdai_status'] = 'Moderate Disease Activity';
+                    body['cdai_color'] = 'lightred';
+            
+                } 
+                else if(req.body.cdai > 22){
+                    body['cdai_status'] = 'High Disease Activity';
+                    body['cdai_color'] = 'red';
+            
+                } 
+            }
+            else {
+                if(req.body.cdai < 2.8){
+                    body['cdai_status'] = 'المرض ساكن (غير نشط)';
+                    body['cdai_color'] = 'green';
+            
+                    
+                } 
+                else if(req.body.cdai > 2.8){
+                    body['cdai_status'] = 'Low نشاط المرض';
+                    body['cdai_color'] = 'yellow';
+            
+                } 
+                else if(req.body.cdai > 10){
+                    body['cdai_status'] = 'Moderate نشاط المرض';
+                    body['cdai_color'] = 'lightred';
+            
+                } 
+                else if(req.body.cdai > 22){
+                    body['cdai_status'] = 'High نشاط المرض';
+                    body['cdai_color'] = 'red';
+            
+                } 
+            }
+                 body['dates'] = today;
+            
+        
+            console.log(req.body)
+        
+            pool.query(`insert into reports set ?`,body, (err, result) => {
+                if(err) {
+                    console.log(err)
+        
+                    res.json({
+                        status:500,
+                        type : 'error',
+                        description:err
+                    })
+                }
+                else {
+                    console.log(result)
+                    res.json({
+                        status:200,
+                        type : 'success',
+                        description:'successfully update',
+                        result:req.body.cdai,
+                        color:req.body.cdai_color,
+                        text:req.body.cdai_status
+                    })
+        
+                    
+                }
+            })
+            
+
         }
     })
+
     
 })
 
@@ -250,5 +732,23 @@ router.post('/getdatebyreport',(req,res)=>{
             }
     })
 })
+
+
+router.post('/check-date',(req,res)=>{
+    pool.query(`select * from reports where dates = '${req.body.date}' and userid = '${req.body.userid}'`,(err,result)=>{
+        if(err) throw err;
+        else if(result[0]){
+          res.json({
+              msg : 'success'
+          })
+        }
+        else{
+            res.json({
+                msg : 'failed'
+            })
+        }
+    })
+})
+
 
 module.exports = router;
